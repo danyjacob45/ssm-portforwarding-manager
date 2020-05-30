b056753b56028ae1cec69ac0f4880efbd39e0789
Dany Jacob <danyjacob45@gmail.com>
2020-05-30 20:43:57 +0800

    add changes to enable port connections

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, make_response, jsonify
+from flask import Flask, render_template, make_response, jsonify, request
 import requests
-
+import ssm
 
 app = Flask(__name__)
 
@@ -14,14 +14,20 @@ def jsonparser():
     url = "https://jsonplaceholder.typicode.com/todos/"
     response = requests.get(url)
     json_dict = response.json()
-    column_names = ['userid','id','title','completed']
+    column_names = ['userId','id','title','completed']
     return render_template('record.html', records=json_dict, colnames=column_names)
 
-@app.route('/connect', methods = ['GET'])
+@app.route('/connect', methods = ['GET', 'POST'])
 def connect():
     print("connect pressed")
+    port = request.args['port']
+    id = request.args['id']
+    platform = request.args['title']
+    profile = request.args['completed']
+    #ssm.executeSSM(id,platform,profile,port)
+    return "Hello from port# " + port
 
-@app.route('/disconnect', methods = ['GET'])
+@app.route('/disconnect', methods = ['GET','POST'])
 def disconnect():
     print("disconnect pressed")
 
--- ssm.py
+++ ssm.py
@@ -1,39 +1,44 @@
 import boto3
 import socketserver
 
-def executeSSM( instance_id, platform, profile ):
-    with socketserver.TCPServer(("localhost", 0), None) as s:
-        free_port = s.server_address[1]
-    boto3.setup_default_session(profile_name=profile)
-    client = boto3.client('ssm', region_name='ap-southeast-1')
-    if platform == "windows":
-        response = client.start_session(
-            Target=instance_id,
-            DocumentName='AWS-StartPortForwardingSession',
-            Parameters={
-                'portNumber': [
-                    '3389',
-                ],
-                'localPortNumber': [
-                    str(free_port),
-                ]
-            }
-        )
-    if platform == "linux":
-        response = client.start_session(
-            Target=instance_id,
-            DocumentName='AWS-StartPortForwardingSession',
-            Parameters={
-                'portNumber': [
-                    '22',
-                ],
-                'localPortNumber': [
-                    str(free_port),
-                ]
-            }
-        )
-    print (response)
-    print (free_port)
-    return response,free_port
+AWS_CONNECTED = False
+
+
+def executeSSM( instance_id, platform, profile,port=0):
+    if AWS_CONNECTED:
+        with socketserver.TCPServer(("localhost", 0), None) as s:
+            free_port = s.server_address[1]
+        boto3.setup_default_session(profile_name=profile)
+        client = boto3.client('ssm', region_name='ap-southeast-1')
+        if platform == "windows":
+            response = client.start_session(
+                Target=instance_id,
+                DocumentName='AWS-StartPortForwardingSession',
+                Parameters={
+                    'portNumber': [
+                        '3389',
+                    ],
+                    'localPortNumber': [
+                        str(free_port),
+                    ]
+                }
+            )
+        if platform == "linux":
+            response = client.start_session(
+                Target=instance_id,
+                DocumentName='AWS-StartPortForwardingSession',
+                Parameters={
+                    'portNumber': [
+                        '22',
+                    ],
+                    'localPortNumber': [
+                        str(free_port),
+                    ]
+                }
+            )
+        print (response)
+        print (free_port)
+        return response,free_port
+
 
 executeSSM("i-01f747b2283fde7d3","linux","network")
