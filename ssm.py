9c35d9ec6d04b27789fba17d2a7d2a8212435586
ebinpissac <ebinpissac@gmail.com>
2020-05-30 22:46:45 +0800

    working mostly

--- ssm.py
+++ ssm.py
@@ -1,53 +1,27 @@
 import boto3
 import socketserver
 import websocket
-AWS_CONNECTED = False
+import subprocess
+import os 
 
+
+
+AWS_CONNECTED = True
 
 
 def executeSSM(instance_id, platform, profile, port=0):
     if AWS_CONNECTED:
         with socketserver.TCPServer(("localhost", 0), None) as s:
             free_port = s.server_address[1]
-        boto3.setup_default_session(profile_name=profile)
-        client = boto3.client('ssm', region_name='ap-southeast-1')
         if platform == "windows":
-            response = client.start_session(
-                Target=instance_id,
-                DocumentName='AWS-StartPortForwardingSession',
-                Parameters={
-                    'portNumber': [
-                        '3389',
-                    ],
-                    'localPortNumber': [
-                        str(free_port),
-                    ]
-                }
-            )
+            connect_parameters="portNumber=3389,localPortNumber=" + str(free_port) + ""
         if platform == "linux":
-            response = client.start_session(
-                Target=instance_id,
-                DocumentName='AWS-StartPortForwardingSession',
-                Parameters={
-                    'portNumber': [
-                        '22',
-                    ],
-                    'localPortNumber': [
-                        str(free_port),
-                    ]
-                }
-            )
-
-        ws = websocket.WebSocket()
-        ws.connect("ws://example.com/websocket", http_proxy_host="proxy_host_name", http_proxy_port=3128)
-
-        print(response)
-        print(free_port)
-        return response, free_port
-
-
+            connect_parameters="portNumber=22,localPortNumber=" + str(free_port) + ""
+        aws_ssm_start = "aws ssm --profile "+ profile +" start-session --target " + instance_id + " --document-name AWS-StartPortForwardingSession --parameters " + connect_parameters
+        subprocess.Popen(aws_ssm_start)
+        return free_port
 def setup():
-    a = ["dltest", "dlprod"]  # to be loaded from a config if possible
+    a = ["dltest", "dlprod"] #to be loaded from a config if possible
     list_info = []
     for x in range(len(a)):
         boto3.setup_default_session(profile_name=a[x])
@@ -62,21 +36,18 @@ def setup():
     print(list_info)
     return list_info
 
-
 def setupDummy():
-    a = ["dltest", "dlprod"]  # to be loaded from a config if possible
+    a = ["dltest", "dlprod"] #to be loaded from a config if possible
     list_info = []
     my_json = {}
     for x in range(len(a)):
+
         for instance in range(2):
             my_json["profile"] = a[x]
-            my_json['instance_id'] = instance * 1
-            my_json['platform'] = instance * 1
+            my_json['instance_id'] = instance*1
+            my_json['platform'] = instance*1
             # print (instance.id , instance.platform)
             my_json['tag_tame'] = "Value"
             list_info.append(my_json)
     print(list_info)
     return list_info
-
-
-executeSSM("i-01f747b2283fde7d3", "linux", "network")
