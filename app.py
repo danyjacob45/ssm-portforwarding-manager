0d71d5e95df041bd30944cef8a60d52da7a38a60
Dany Jacob <danyjacob45@gmail.com>
2020-05-30 17:07:43 +0800

    add simple table

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, jsonify
+import requests
+
 
 app = Flask(__name__)
 
@@ -7,6 +9,13 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
+@app.route('/test', methods = ['GET'])
+def jsonparser():
+    url = "https://jsonplaceholder.typicode.com/todos/"
+    response = requests.get(url)
+    json_dict = response.json()
+    column_names = ['userid','id','title','completed']
+    return render_template('record.html', records=json_dict, colnames=column_names)
 
 if __name__ == '__main__':
 
